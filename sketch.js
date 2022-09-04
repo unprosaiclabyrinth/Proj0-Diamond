680f7646e79ca08b6067465e5b027018b391a88a
Himanshu Dongre <hdongr2@uic.edu>
2022-09-04 15:03:05 -0500

    Added animation

--- sketch.js
+++ sketch.js
@@ -5,27 +5,30 @@ const t = D / 8;
 const b = 7 * t;
 const c = D / 2;
 
+let i = 0;
+
 function setup() 
 {
     createCanvas(D, D);
+    background(0);
 }
 
 function draw() 
 {
-    background(0);
     let blue = true;
-    for (let i = 0; i <= c - t; i += s) 
-    {
-        if (blue) {
-            stroke(c - t - i, i, 255);
-            blue = false;
-        } else {
-            stroke(255, i, c - t - i);
-            blue = true;
-        }
-        line(c, t + i, c + i, c);
-        line(c - i, c, c, t + i);
-        line(c, b - i, c - i, c);
-        line(c + i, c, c, b - i);
+    if (blue) {
+        stroke(c - t - i, i, 255);
+        blue = false;
+    } else {
+        stroke(255, i, c - t - i);
+        blue = true;
     }
+    if (i > c - t) {
+        noLoop();
+    }
+    line(c, t + i, c + i, c);
+    line(c - i, c, c, t + i);
+    line(c, b - i, c - i, c);
+    line(c + i, c, c, b - i);
+    i += s;
 }
